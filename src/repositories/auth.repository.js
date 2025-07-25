98b79d49c8d19b00ba85e153a844d6356698d1f1
Imanul Aufa <imanulaufa@gmail.com>
2025-07-25 20:36:24 +0700

    feat(package) : add some package

--- src/repositories/auth.repository.js
+++
@@ -1,6 +0,0 @@
-const db = require('../db/pg');
-exports.createUser = (name, email, hashed, profileImageUrl) => {
-
-  db.query('INSERT INTO users(email, password, name, profile_image) VALUES($1,$2,$3, $4)', [email, hashed, name, profileImageUrl]);}
-exports.findUserByEmail = (email) =>
-  db.query('SELECT * FROM users WHERE email=$1', [email]).then(r => r.rows[0]);
--- src/repositories/profile.repository.js
+++
@@ -1,6 +0,0 @@
-const db = require('../shared/db/pg'); // koneksi PG Pool
-
-exports.updateProfileImage = async (userId, profileImage) => {
-  const query = `UPDATE users SET profile_image = $1 WHERE id = $2`;
-  await db.query(query, [profileImage, userId]);
-};
