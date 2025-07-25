79a36560ab63414706aa20b9825efed8f0c9bbc9
Imanul Aufa <imanulaufa@gmail.com>
2025-07-25 20:46:51 +0700

    fix(db file): fix logic for railways deploy

--- src/db/pg.js
+++ src/db/pg.js
@@ -1,12 +1,12 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, 
-    ssl: { rejectUnauthorized: false } // railway
-// ssl : false
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // railway
+  // ssl: false,
 });
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
-  pool, 
+  pool,
 };
