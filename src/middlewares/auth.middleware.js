8f13b5e3261e6fe5553d6c39d49616203e1b2135
Imanul Aufa <imanulaufa@gmail.com>
2025-07-25 20:47:45 +0700

    fix(auth  middleware): fix token generated

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,15 +1,15 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   const auth = req.headers.authorization;
-  if (!auth) return res.status(401).json({ message: 'Token required' });
+  if (!auth) return res.status(401).json({ message: "Token required" });
 
   try {
-    const token = auth.split(' ')[1];
+    const token = auth.split(" ")[1];
     const user = jwt.verify(token, process.env.JWT_SECRET);
     req.user = user;
     next();
   } catch {
-    res.status(401).json({ message: 'Invalid token' });
+    res.status(401).json({ message: "Invalid token" });
   }
 };
