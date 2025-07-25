6f97c684c10ce7645e159f7b99b91131057c5707
Imanul Aufa <imanulaufa@gmail.com>
2025-07-25 07:24:02 +0700

    feat(servicee controller): add service controller

---
+++ src/controllers/service.controller.js
@@ -0,0 +1,23 @@
+const serviceService = require('../services/service.service');
+
+const getServiceList = async (req, res) => {
+  try {
+    const services = await serviceService.getServices();
+
+    res.status(200).json({
+      status: true,
+      message: 'Get service list success',
+      data: services,
+    });
+  } catch (error) {
+    console.error('Error get services:', error);
+    res.status(500).json({
+      status: false,
+      message: 'Internal Server Error',
+    });
+  }
+};
+
+module.exports = {
+  getServiceList,
+};
