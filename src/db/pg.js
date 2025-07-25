df0ff332f5ca82797c30d70cd6d7fccb7ff5b3f7
Imanul Aufa <imanulaufa@gmail.com>
2025-07-25 07:26:26 +0700

    feat(database connect): add database conncetion, db pool

---
+++ src/db/pg.js
@@ -0,0 +1,12 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL, 
+  //   ssl: { rejectUnauthorized: false } // railway
+ssl : false
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+  pool, 
+};
