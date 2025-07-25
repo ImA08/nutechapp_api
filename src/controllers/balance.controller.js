61ddc970b2945d118a706f2d86cbaf9de9cb0308
Imanul Aufa <imanulaufa@gmail.com>
2025-07-25 20:42:43 +0700

    fix(balance controller) : fix some logic, separating service and controller

--- src/controllers/balance.controller.js
+++ src/controllers/balance.controller.js
@@ -1,9 +1,9 @@
-const balanceService = require('../services/balance.service');
+const balanceService = require("../services/balance.service");
 exports.getBalance = async (req, res) => {
   try {
     const balance = await balanceService.getBalance(req.user.id);
     res.json({ status: true, data: { balance } });
   } catch {
-    res.status(500).json({ status: false, message: 'Failed' });
+    res.status(500).json({ status: false, message: "Failed" });
   }
 };
