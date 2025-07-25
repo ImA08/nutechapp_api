fbaf379d47c34b6a45a0f133186860368b3c91e0
Imanul Aufa <imanulaufa@gmail.com>
2025-07-25 08:38:41 +0700

    feat(app.js): add route for railways

--- src/app.js
+++ src/app.js
@@ -4,9 +4,14 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("NutechPay API is running");
+  });
+  
 const routes = require('./routes');
 app.use(routes);
 app.use('/uploads', express.static('uploads'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server jalan di port ${PORT}`));
+  
