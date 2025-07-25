f70d82208069e1f918cd3ad5c86871edd9498fcb
Imanul Aufa <imanulaufa@gmail.com>
2025-07-25 07:17:14 +0700

    feat(app.js): add file app.js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+const routes = require('./routes');
+app.use(routes);
+app.use('/uploads', express.static('uploads'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server jalan di port ${PORT}`));
