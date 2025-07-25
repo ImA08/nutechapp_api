61e40f709eb3903db46ee39d12fe85433a86167b
Imanul Aufa <imanulaufa@gmail.com>
2025-07-25 20:48:46 +0700

    fix(upload mddleware): change request protocol to base url

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -1,9 +1,8 @@
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
-// Pastikan folder uploads ada
-const uploadDir = path.join(__dirname, '../../uploads');
+const uploadDir = path.join(__dirname, "../../uploads");
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir);
 }
@@ -15,7 +14,7 @@ const storage = multer.diskStorage({
   filename: function (req, file, cb) {
     const uniqueName = `${Date.now()}-${file.originalname}`;
     cb(null, uniqueName);
-  }
+  },
 });
 
 const upload = multer({ storage });
